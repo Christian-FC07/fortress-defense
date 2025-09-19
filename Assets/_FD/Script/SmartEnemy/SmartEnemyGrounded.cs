e98d4fcbd511faf2462ec56a42030b8c04d05c12
Rasul Tahmasebi <mohammad.rasul.tahmasebi@gmail.com>
2025-09-19 18:41:09 +0330

    grave feature removed

--- Assets/_FD/Script/SmartEnemy/SmartEnemyGrounded.cs
+++ Assets/_FD/Script/SmartEnemy/SmartEnemyGrounded.cs
@@ -35,6 +35,9 @@ public class SmartEnemyGrounded : Enemy, ICanTakeDamage, IGetTouchEvent
     EnemyCallMinion callMinion;
     SpawnItemHelper spawnItem;
 
+    [Header("Other")]
+    //public bool isPet;
+
 
     [Space(3)] [Header("Spawning from Underground")]
     public bool spawnFromUnderground;
@@ -55,7 +58,6 @@ public class SmartEnemyGrounded : Enemy, ICanTakeDamage, IGetTouchEvent
     private float _zPos;
     public GameObject shadow;
     private float _initialMoveSpeed;
-
 
     IEnumerator Climb()
     {
@@ -415,6 +417,7 @@ public class SmartEnemyGrounded : Enemy, ICanTakeDamage, IGetTouchEvent
                     else if (!meleeAttack.isAttacking && enemyState == ENEMYSTATE.ATTACK)
                     {
                         SetEnemyState(ENEMYSTATE.WALK);
+                        SetSkeletonAnimation(ANIMATION_STATE.WALK, true);
                     }
                 }
 
@@ -488,6 +491,11 @@ public class SmartEnemyGrounded : Enemy, ICanTakeDamage, IGetTouchEvent
 
     public override void Die()
     {
+        if (isPet)
+        {
+            SetSkeletonAnimation(ANIMATION_STATE.WALK, true);
+            Debug.Log("hello");
+        }
         if (isDead)
             return;
 
@@ -577,17 +585,17 @@ public class SmartEnemyGrounded : Enemy, ICanTakeDamage, IGetTouchEvent
             SpawnSystemHelper.GetNextObject(disableFX, true).transform.position =
                 spawnDisableFX != null ? spawnDisableFX.position : transform.position;
 
-        GameObject grave = SpawnSystemHelper.GetNextObject(LevelEnemyManager.Instance.GraveHit, true);
-        grave.SetActive(true);
-        grave.transform.position = new Vector3(transform.position.x, transform.position.y + 0.55f, transform.position.z);
-        grave.transform.GetChild(0).TryGetComponent(out Animator animator);
-        if (animator != null) {
+        //GameObject grave = SpawnSystemHelper.GetNextObject(LevelEnemyManager.Instance.GraveHit, true);
+        //grave.SetActive(true);
+        //grave.transform.position = new Vector3(transform.position.x, transform.position.y + 0.55f, transform.position.z);
+        //grave.transform.GetChild(0).TryGetComponent(out Animator animator);
+        /*if (animator != null) {
             animator.SetTrigger("play");
-        }
+        }*/
         
-        yield return new WaitForSeconds(2);
-        grave.transform.position = new Vector3(0, 100, grave.transform.position.z);
-        grave.SetActive(false);
+        yield return new WaitForSeconds(0.1f);
+        //grave.transform.position = new Vector3(0, 100, grave.transform.position.z);
+        //grave.SetActive(false);
 
         gameObject.SetActive(false);
     }
