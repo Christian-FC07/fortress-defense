5c3040ea48765193d9270ce12d9b16287769b0f6
m.Yasin.Frz <firooziyasin90@gmail.com>
2025-11-06 14:53:02 +0330

    Comics FIx 1

--- Assets/Atlas games/Scripts/Comics/ShowComic.cs
+++ Assets/Atlas games/Scripts/Comics/ShowComic.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class ShowComic : MonoBehaviour
 {
     [Header("Refrences (Be to be changed)")]
-    public Comics comics;
+    public Comics Comics;
     public GameObject UI;
     public GameObject blur;
     public GameObject archerManager;
@@ -14,105 +14,85 @@ public class ShowComic : MonoBehaviour
     GameObject newComicClone;
     public float timer;
     public GameObject[] menuParts;
-    public showTutorial tutorialManager;
-    public float tutorialDelayAfterComic = 2f;
-
 
     public static bool isComicOn = false;
     public static bool isComicOff2 = false;
-    public  bool isComicEnded = false;
 
     public void Start()
     {
         scene = SceneManager.GetActiveScene();
         ResetComicsFlags();
-        isComicEnded=false;
     }
-
-    private bool tutorialTimerActive = false;
-    private float tutorialTimer = 0f;
-    private bool tutorialSpawned = false; // برای جلوگیری از لود چندباره
 
     public void Update()
     {
         timer += Time.deltaTime;
 
-        // 🎬 1. Comic را لود کن
-        if (!isComicOn)
+        for (int i = 0; i < Comics.infoT.Length; i++)
         {
-            for (int i = 0; i < comics.infoT.Length; i++)
-            {
-                int levelRef = comics.infoT[i].LevelNumber;
-                var comicObj = comics.infoT[i].ComicPrefab;
-                int delayTime = (int)comics.infoT[i].Delay;
-                var modelRef = comics.infoT[i].Model.ToString();
+            int levelRef = Comics.infoT[i].LevelNumber;
+            var comicObj = Comics.infoT[i].ComicPrefab;
+            int delayTime = (int)Comics.infoT[i].Delay;
+            var modelRef = Comics.infoT[i].Model.ToString();
+            var partRef = Comics.infoT[i].MenuPart.ToString();
 
-                if (modelRef == "InGame" && scene.name == "Playing atlas")
+            // ================= InGame Tutorials =================
+            if (modelRef == "InGame" && scene.name == "Playing atlas")
+            {
+                if (levelRef == GlobalValue.levelPlaying && isComicOn == false)
                 {
-                    if (levelRef == GlobalValue.levelPlaying && timer > delayTime)
+                    if (timer > delayTime)
                     {
                         newComicClone = Instantiate(comicObj, transform.position, Quaternion.identity);
                         UI.transform.localScale = new Vector2(2, 2);
                         archerManager.SetActive(false);
                         blur.SetActive(true);
+
                         isComicOn = true;
-                        Debug.Log("🎞 Comic spawned");
                     }
-                }
-            }
-        }
 
-        // 🧩 2. وقتی دکمه‌ی Comic زده شد => Comic بسته شود
-        if (isComicOn && buttonCheck.Comicpress)
-        {
-            if (newComicClone != null)
-                Destroy(newComicClone, 0.1f);
-
-            UI.transform.localScale = new Vector2(1, 1);
-            archerManager.SetActive(true);
-            blur.SetActive(false);
-
-            isComicOn = false;
-            isComicOff2 = true;
-            isComicEnded = true;
-            buttonCheck.Comicpress = false;
-
-            // فعال‌سازی تایمر برای Tutorial
-            tutorialTimerActive = true;
-            tutorialTimer = 0f;
-
-            Debug.Log("✅ Comic closed, tutorial timer started");
-        }
-
-        // ⏳ 3. صبر کن تا تایمر بگذره و سپس Tutorial را لود کن
-        if (tutorialTimerActive && !tutorialSpawned)
-        {
-            tutorialTimer += Time.deltaTime;
-            if (tutorialTimer > tutorialDelayAfterComic)
-            {
-                // لود Tutorial
-                for (int i = 0; i < comics.infoT.Length; i++)
-                {
-                    int levelRef = comics.infoT[i].LevelNumber;
-                    var tutorialObj = comics.infoT[i].ComicPrefab; // فرض بر این است که هر Comic، Tutorial متناظر دارد
-                    var modelRef = comics.infoT[i].Model.ToString();
-
-                    if (modelRef == "InGame" && scene.name == "Playing atlas")
+                    if (ComicbuttonCheck.press)
                     {
-                        if (levelRef == GlobalValue.levelPlaying && tutorialObj != null)
-                        {
-                            Instantiate(tutorialObj, transform.position, Quaternion.identity);
-                            UI.transform.localScale = new Vector2(2, 2);
-                            archerManager.SetActive(false);
-                            blur.SetActive(true);
-                            tutorialSpawned = true;
-                            tutorialTimerActive = false;
-                            Debug.Log("🎯 Tutorial spawned after Comic");
-                            break;
-                        }
+                        Destroy(newComicClone);
+                        ComicbuttonCheck.press = false;
                     }
+
+
+                }
+                else if (isComicOff2 == false && ComicbuttonCheck.press)
+                {
+                    Destroy(newComicClone, 0.1f);
+                    Time.timeScale = 1;
+                    UI.transform.localScale = new Vector2(1, 1);
+                    archerManager.SetActive(true);
+                    blur.SetActive(false);
+
+                    isComicOff2 = true;
+                    ComicbuttonCheck.press = false;
                 }
             }
+            // ================= InMenu Tutorials =================
+            //else if (modelRef == "InMenu" && scene.name == "Menu atlas Test")
+            //{
+            //    string tutorialKey = "tutorialMenu_" + partRef;
+
+            //    if (PlayerPrefs.GetInt(tutorialKey, 0) == 0)
+            //    {
+            //        if (partRef == GlobalValue.menuPart && timer > delayTime)
+            //        {
+            //            newComicClone = Instantiate(comicObj, transform.position, Quaternion.identity);
+
+            //            PlayerPrefs.SetInt(tutorialKey, 1);
+            //            PlayerPrefs.Save();
+            //        }
+            //    }
+
+            //    if (ComicbuttonCheck.press)
+            //    {
+            //        Destroy(newComicClone);
+            //        ComicbuttonCheck.press = false;
+            //    }
+            //}
         }
     }
 
@@ -123,7 +103,6 @@ public class ShowComic : MonoBehaviour
     {
         isComicOn = false;
         isComicOff2 = false;
-        isComicEnded = false;
         timer = 0f;
     }
 }
